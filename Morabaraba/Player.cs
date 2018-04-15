454ad63a455eb0e495375b15cb583ab6a32ce8d5
Sakhele <molweni200@yahoo.com>
2018-04-15 20:35:30 +0200

    Create getNumberOfCows method for Player class

--- Morabaraba/Player.cs
+++ Morabaraba/Player.cs
@@ -43,7 +43,7 @@ namespace Morabaraba
 
         public int numberOfCows()
         {
-            throw new NotImplementedException();
+            return cowsLeft;
         }
 
         public void RemoveMill()
