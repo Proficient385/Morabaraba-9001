509397eebb9028a2dd364894a9cfbd6835d83cf3
Ndhuvazi Mkansi <g16M6496@ict.ru.ac.za>
2018-04-15 17:36:55 +0200

    Add IBoard interface

--- Morabaraba/Interfaces.cs
+++ Morabaraba/Interfaces.cs
@@ -4,38 +4,44 @@ using System.Text;
 
 namespace Morabaraba
 {
-    
-        public interface IPlayer
-        {
-            int numberOfCows();
-            void AddMills();
-            void RemoveMill();
-            void updatePlayed();
-            string currentplayer();
-            List<string> getPlayedPos();
-            List<List<string>> getMills();
-            string getState();
-            void updateState();
-        }
 
-        public interface ICommandLineInterface
-        {
-            void printBoard();
-            void printPlayerTurn(string currentPlayer);
-
-            void printInvalidKill();
-            void invalidMove();
-            void printGameState();
-        }
-
-        public interface IReferee
-        {
-            bool IsDraw();
-            void play();
-            void Winner();
-            bool isMill();
-        }
-
-
+    public interface IBoard
+    {
+        void updateBoard();
+        void printBoard();
     }
+
+    public interface IPlayer
+    {
+        int numberOfCows();
+        void AddMills();
+        void RemoveMill();
+        void updatePlayed();
+        string currentplayer();
+        List<string> getPlayedPos();
+        List<List<string>> getMills();
+        string getState();
+        void updateState();
+    }
+
+    public interface ICommandLineInterface
+    {
+        void printBoard();
+        void printPlayerTurn(string currentPlayer);
+
+        void printInvalidKill();
+        void invalidMove();
+        void printGameState();
+    }
+
+    public interface IReferee
+    {
+        bool IsDraw();
+        void play();
+        void Winner();
+        bool isMill();
+    }
+
+
+}
 
